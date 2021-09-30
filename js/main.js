789f3d1adbf3db4cf456aa859da6c473d0acd6cc
Karina <kbusinessk@gmail.com>
2021-09-30 18:49:59 +1000

    fixed bugs

--- js/main.js
+++ js/main.js
@@ -1,8 +1,6 @@
 // https://devdocs.io/javascript/global_objects/math/random
 
-// Option number 1
-
-const getRandomNumber = (min, max) => {
+const getRandomInteger = (min, max) => {
   if (min < 0 || min >= max) {
     throw new Error('Неверно указано начало диапазона');
   }
@@ -10,25 +8,8 @@ const getRandomNumber = (min, max) => {
   return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min));
 };
 
-getRandomNumber(0, 140);
+getRandomInteger(0, 140);
 
-const checkCommentLength = (comment, maxLength) => comment.length <= maxLength;
+const checkTextLength = (text, maxLength) => text.length <= maxLength;
 
-checkCommentLength('Проверка длины введённого комментария', 140);
-
-//Option number 2
-
-// function getRandomNumber(min, max, precision) {
-//   if (min < 0 || min >= max) {
-//     throw new Error('Неверно указано начало диапазона ')
-//   }
-//   console.log(Number(Math.random() * (max - min) + min).toFixed(precision))
-// }
-
-// getRandomNumber(0, 140)
-
-// function checkCommentLength(comment, maxLength = 140) {
-//   return comment.length <= maxLength
-// }
-
-// console.log(checkCommentLength('Проверка длины введённого комментария'))
+checkTextLength('Проверка длины введённого комментария', 140);
