96b542d331ddec29e0fa6b31069e0c682ea3a6a6
Karina <kbusinessk@gmail.com>
2021-10-15 18:12:40 +1000

    Отрисуй меня полностью (часть 1)fixed

--- js/pictures.js
+++ js/pictures.js
@@ -2,16 +2,15 @@ import { generateUserPhotos } from './util.js';
 
 const userPhotos = generateUserPhotos();
 const pictureContainer = document.querySelector('.pictures');
-const templateFragment = document.querySelector('#picture').content;
-const template = templateFragment.querySelector('a');
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
 const fragment = document.createDocumentFragment();
 
-userPhotos.forEach((userPhoto) => {
-  const element = template.cloneNode(true);
-  element.children[0].src = userPhoto['url'];
-  element.children[1].querySelector('.picture__likes').textContent = userPhoto['likes'];
-  element.children[1].querySelector('.picture__comments').textContent =
-    userPhoto['comments'].length;
+userPhotos.forEach(({ url, likes, comments }) => {
+  const element = pictureTemplate.cloneNode(true);
+  element.querySelector('.picture__img').src = url;
+  element.querySelector('.picture__likes').textContent = likes;
+  element.querySelector('.picture__comments').textContent = comments.length;
   fragment.appendChild(element);
 });
 
